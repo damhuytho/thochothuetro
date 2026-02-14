bc1ef0f12876a36b18552bce3ffd4b49d1161000
damhuytho <damhuytho@gmail.com>
2026-02-14 12:16:58 +0700

    Cap nhat layout Mobile va fix loi ban do

--- public/sw.js
+++ public/sw.js
@@ -8,8 +8,6 @@ const DATA_CACHE_NAME = 'tho-data-v8';
 
 const STATIC_ASSETS = [
     '/logo.png',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
 ];
 
