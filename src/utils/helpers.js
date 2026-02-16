4b10bfcfd5b4822d2226cdaf9cbe85615ef7f5b8
damhuytho <damhuytho@gmail.com>
2026-02-16 13:52:53 +0700

    Cap nhat layout Mobile va fix loi ban do

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,17 +2,20 @@
 
 export function formatPrice(price) {
     if (!price) return 'Liên hệ';
-    if (price >= 1000000) return (price / 1000000).toString() + " Triệu";
+    // [Gợi ý nhỏ] Sửa lại chỗ này một chút để hiển thị đẹp hơn (VD: 3.5 thay vì 3.500000)
+    if (price >= 1000000) {
+        let p = price / 1000000;
+        // Làm tròn tối đa 1 số lẻ để gọn (VD: 3.5 Triệu)
+        return parseFloat(p.toFixed(1)).toString() + " Triệu"; 
+    }
     return new Intl.NumberFormat('vi-VN').format(price);
 }
 
-export function hideNumber(address) {
-    if (!address) return "";
-    return address.replace(/^[0-9][0-9a-zA-Z\/,.-]*\s+/, '');
-}
+// [ĐÃ XÓA] export function hideNumber(address) ...
 
 export function getDistrictSlug(district) {
     if (!district) return '';
+    // Logic này hơi cứng nhắc (hardcode), nhưng nếu web chỉ có 2 quận thì OK
     return district === 'Tân Bình' ? 'tan-binh' : 'phu-nhuan';
 }
 
